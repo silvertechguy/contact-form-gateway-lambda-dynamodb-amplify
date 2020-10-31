5c5edee0fed6158d722a03e264427b92187de71a
Ali Fadda <silvertechguy@gmail.com>
2020-10-31 15:54:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Container, Button, Form } from "react-bootstrap";
+import Amplify, { API } from "aws-amplify";
+import awsExports from "./aws-exports";
+import { useState } from "react";
+Amplify.configure(awsExports);
 
 function App() {
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+
+  const updateFormState = (e) => {
+    setForm((pre) => ({ ...pre, [e.target.name]: e.target.value }));
+  };
+
+  const addContact = async () => {
+    const data = {
+      body: { name: form.name, email: form.email, message: form.message },
+    };
+
+    const apiData = await API.post("apicontactform", "/contact", data);
+    console.log(apiData);
+    alert("Mail sent");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div>
+        <h3>Get in touch</h3>
+        <br />
+        <Form>
+          <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              placeholder="Name"
+              name="name"
+              value={form.name}
+              onChange={updateFormState}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              placeholder="Email"
+              name="email"
+              value={form.email}
+              onChange={updateFormState}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Message</Form.Label>
+            <Form.Control
+              placeholder="Message"
+              name="message"
+              value={form.message}
+              onChange={updateFormState}
+            />
+          </Form.Group>
+          <Button onClick={addContact}>Send a message</Button>
+        </Form>
+      </div>
+    </Container>
   );
 }
 
